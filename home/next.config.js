5a989615944936b2a7f8acec83b3346ee6203e92
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-06-17 15:06:26 +0200

    test

--- home/next.config.js
+++ home/next.config.js
@@ -1,23 +1,28 @@
-const { BLOG_URL } = process.env
+const {BLOG_URL} = process.env
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      // {
-      //   source: '/:path*',
-      //   destination: `/:path*`,
-      // },
-      {
-        source: '/blog',
-        destination: `${BLOG_URL}/blog`,
-      },
-      {
-        source: '/blog/:path*',
-        destination: `${BLOG_URL}/blog/:path*`,
-      },
-    ]
-  },
+	trailingSlash: false,
+	async rewrites() {
+		return [
+			{
+				source: '/',
+				destination: '/',
+			},
+			{
+				source: '/:path*',
+				destination: `/:path*`,
+			},
+			{
+				source: '/blog',
+				destination: `${BLOG_URL}/blog`,
+			},
+			{
+				source: '/blog/:path*',
+				destination: `${BLOG_URL}/blog/:path*`,
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
